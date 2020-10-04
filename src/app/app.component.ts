6c386fa7c344d5f28a76e0cc0773b9256f720f52
Nassima-L <hopi-52@hotmail.com>
2020-10-04 10:33:59 +0100

    auth commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dropedetions-test';
+  constructor(){
+  const config = {
+      apiKey: "AIzaSyD9V1dT4aEIspe4LGrRD3ti1vUscHcVHG0",
+      authDomain: "biblio-5b2f9.firebaseapp.com",
+      databaseURL: "https://biblio-5b2f9.firebaseio.com",
+      projectId: "biblio-5b2f9",
+      storageBucket: "biblio-5b2f9.appspot.com",
+      messagingSenderId: "25812641816",
+      appId: "1:25812641816:web:f966e8b70d364d945bbb6d"
+
+    };
+    firebase.initializeApp(config);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
+//modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+//component
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+//services
+import { AuthService } from './services/auth.service';
+import { AuthGuardService} from './services/auth-guard.service';
+import { Routes } from '@angular/router';
+import { BodyListComponent } from './private-section-list/body-list/body-list.component';
+import { NavListComponent } from './private-section-list/nav-list/nav-list.component';
+
+
+const appRoutes: Routes =[
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component : SigninComponent },
+  
+  { path:'' , redirectTo: 'private-section-list/body-list', pathMatch: 'full'},
+  { path:'**', redirectTo:'private-section-list/body-list'}
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    SigninComponent,
+    BodyListComponent,
+    NavListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ AuthService,AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
